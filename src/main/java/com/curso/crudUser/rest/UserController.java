9015261ac1f4d9e1283f60e0bcea4a4b90175e31
EnriqueSala <sala488@gmail.com>
2023-02-10 01:30:07 -0600

    added cors support

--- src/main/java/com/curso/crudUser/rest/UserController.java
+++ src/main/java/com/curso/crudUser/rest/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.curso.crudUser.entity.User;
 import com.curso.crudUser.service.UserService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class UserController {
